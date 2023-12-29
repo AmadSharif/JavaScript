1b6bf83b760a2704a459a175f02f729a0aaa97a6
Amad Sharif <139844237+AmadSharif@users.noreply.github.com>
2023-12-29 06:21:30 -0800

    ok

--- strings.js
+++ strings.js
@@ -1,5 +1,5 @@
-// prompt the user to enter their full name .generate a user name for them based on thee input.
+// prompt the user to enter their full name .generate a user name for them based on the input.
 // start user name with '@' and end with the length of the user name;
 
 let fullName = prompt("Enter you full name without spaces");
-console.log("@"+fullName+fullName.length);
+let userName=console.log("@"+fullName+fullName.length);
