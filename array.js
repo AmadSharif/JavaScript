68c56d3ce37ad9f0a99a44426fddc68a46febdf0
Amad Sharif <139844237+AmadSharif@users.noreply.github.com>
2023-12-31 06:03:03 -0800

    arrays fundamentals

---
+++ array.js
@@ -0,0 +1,19 @@
+// Array of marks
+let marks=[12,44,65,77,90,66]
+console.log(marks);
+
+// storing string values in an array
+let heroes=["thor","antman","hulk","spiderman","ironman"];
+
+// manipulating the values of an array through index;
+heroes[0]="batman";//replaces thor with batman
+
+// printing values of array using for loop
+for(let i=0;i<heroes.length;i++){
+    console.log(heroes[i]);
+}
+// to find the length of the array
+console.log("length of the array 'heroes' =",heroes.length);
+
+// accessing the specific value in an array through index number;
+console.log(heroes[0]);//prints 'thor'
