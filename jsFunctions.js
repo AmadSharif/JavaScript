35650055d427c0ced2c36abacd2252dd57792220
Amad Sharif <139844237+AmadSharif@users.noreply.github.com>
2024-01-02 07:52:42 -0800

    functions in javascript

---
+++ jsFunctions.js
@@ -0,0 +1,35 @@
+// create a function to count the number of vowels in a string;
+
+function countVowels(str) {
+  let count = 0;
+  for (const char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// create an arrow function to perform the same task;
+
+let countVow = (str) => {
+  let count = 0;
+  for (const char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+};
